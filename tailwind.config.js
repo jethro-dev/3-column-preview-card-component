c6c1ba56d27275ac47db8d2b13a4e9b90e7358f2
Jethro Au <supergalong@gmail.com>
2022-01-04 00:43:43 +0000

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "bright-orange": "hsl(31, 77%, 52%)",
+        "dark-cyan": "hsl(184, 100%, 22%)",
+        "very-dark-cyan": "hsl(179, 100%, 13%)",
+        "transparent-white": "hsla(0, 0%, 100%, 0.75)",
+        "very-light-gray": "hsl(0, 0%, 95%)",
+      },
+      fontSize: {
+        paragraph: "15px",
+      },
+    },
+  },
+  plugins: [],
+};
